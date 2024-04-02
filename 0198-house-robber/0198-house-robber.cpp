2900d26903e4dbb61e40c8c01d070ce1a3949f93
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-04-02 10:31:51 +0530

    Time: 2 ms (40.42%), Space: 9.2 MB (33.74%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int solve(vector<int>& nums, vector<int>& memo, int currentHouse) {
+        if(currentHouse >= nums.size()) return 0;
+        
+        if(memo[currentHouse] == -1) {
+            return memo[currentHouse] = max(
+                nums[currentHouse] + solve(nums, memo, currentHouse+2),
+                solve(nums, memo, currentHouse+1)
+            );
+        }
+        return memo[currentHouse];
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int> memo(nums.size(), -1);
+        return max(solve(nums, memo, 0), solve(nums, memo, 1));
+    }
+};
