a400c5de08c18070f2cc3774f7850fa82d2a117a
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-06-21 09:11:46 +0530

    Time: 33 ms (16.95%), Space: 34.3 MB (82.09%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int totalCustomers = 0, unsatisfiedCustomer = 0;
-        vector<int> unsatisfiedCustomers;
-        for(int i = 0; i < grumpy.size(); i++) {
-            unsatisfiedCustomers.push_back(grumpy[i]*customers[i]);
-            totalCustomers += customers[i];
-            unsatisfiedCustomer += unsatisfiedCustomers[i];
-        }
-        
         int left = 0, right = 0, maxSum = 0, currentSum = 0;
-        while(right < minutes) {
-            currentSum += unsatisfiedCustomers[right++];
-        }
         
+        while(right < minutes) {
+            totalCustomers += customers[right];
+            unsatisfiedCustomer += grumpy[right]*customers[right];
+            currentSum += grumpy[right]*customers[right++];
+        }
         for(right = minutes; right < grumpy.size(); right++) {
             maxSum = max(currentSum, maxSum);
-            currentSum += unsatisfiedCustomers[right];
-            currentSum -= unsatisfiedCustomers[left++];
+            unsatisfiedCustomer += grumpy[right]*customers[right];
+            currentSum += grumpy[right]*customers[right];
+            currentSum -= grumpy[left]*customers[left++];
+            totalCustomers += customers[right];
         }
-        
+    
         maxSum = max(currentSum , maxSum);
         return totalCustomers - unsatisfiedCustomer + maxSum;
     }
