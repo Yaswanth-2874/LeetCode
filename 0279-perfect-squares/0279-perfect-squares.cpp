810c44425d560f1b3966cfd6791952ecf7d18e44
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-08-09 21:31:26 +0530

    Time: 232 ms (12.33%), Space: 7.9 MB (92.82%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n < 4)
+            return n;
+        double root = sqrt(n);
+        int largestSquareRoot = floor(root);
+        if(largestSquareRoot == sqrt(n))
+            return 1;
+        int minMoves = n;
+        for(int i = largestSquareRoot; i >= 1; i--) {
+            int currentMoves = n/(i*i);
+            currentMoves += numSquares(n % (i*i));
+            minMoves = min(minMoves, currentMoves);
+        }
+        return minMoves;
+    }
+};
