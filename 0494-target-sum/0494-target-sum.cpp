d09fff05f505d806c2ff0f7819f5bdc10a6ad078
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-08-29 19:22:19 +0530

    Time: 17 ms (47.57%), Space: 24.6 MB (27.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,27 @@
 class Solution {
+    int offset;
+    vector<vector<int>> memo;
 
     int findTotalWays(vector<int>& nums, int target, int sum = 0, int index = 0) {
-        if(index >= nums.size())
+        if (index == nums.size())
             return sum == target;
-        
-        int addCurr = findTotalWays(nums, target, sum+nums[index], index+1);
-        int subCurr = findTotalWays(nums, target, sum-nums[index], index+1);
-        
-        return addCurr + subCurr;
+
+        if (memo[index][sum + offset] != -1)
+            return memo[index][sum + offset];
+
+        int addCurr = findTotalWays(nums, target, sum + nums[index], index + 1);
+        int subCurr = findTotalWays(nums, target, sum - nums[index], index + 1);
+
+        memo[index][sum + offset] = addCurr + subCurr;
+        return memo[index][sum + offset];
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        offset = totalSum;
+
+        memo.assign(nums.size(), vector<int>(2 * totalSum + 1, -1));
+
         return findTotalWays(nums, target);
     }
 };
