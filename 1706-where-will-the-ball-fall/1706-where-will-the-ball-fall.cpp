56f42a344988883734868d377f29c34b12f10270
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-09-06 10:44:47 +0530

    Time: 21 ms (19.84%), Space: 17.1 MB (7.57%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<int>> memo;
+    int findEndCol(vector<vector<int>>& grid, int row, int col) {
+        if(row >= grid.size())
+            return col;
+        if(col >= grid[0].size() || col < 0)
+            return -1;
+        if(memo[row][col] != -2)
+            return memo[row][col];
+        int direction = grid[row][col];
+        if(col + direction >= grid[0].size() || col + direction < 0)
+            return -1;
+        if(grid[row][col] != grid[row][col + direction])
+            return -1;
+        
+        return memo[row][col] = findEndCol(grid, row + 1, col + direction);
+    }
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int> ans;
+        memo = vector<vector<int>> (grid.size(), vector<int> (grid[0].size(), -2));
+        for(int i = 0; i < grid[0].size(); i++)
+            ans.push_back(findEndCol(grid, 0, i));
+        
+        return ans;
+    }
+};
