b9be34eba1463e5914a6c2506ba4bf8983cb34ba
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-09-10 22:10:03 +0530

    Time: 2 ms (52.10%), Space: 7.4 MB (57.44%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(4);
+        
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i % 4] = dp[(i-3)%4] + dp [(i-2)%4] + dp[(i-1)%4];
+        }
+        
+        return dp[n%4];
+    }
+};
