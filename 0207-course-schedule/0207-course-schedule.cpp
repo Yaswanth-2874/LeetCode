ec7575a8e1fb05a75294849b5a395e873eeafa4f
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-09-18 11:49:40 +0530

    Time: 14 ms (75.16%), Space: 18.7 MB (23.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,40 @@
 class Solution {
-   
-    bool topologicalSort(vector<vector<int>>& graph, int vertices, vector<int>& indegree) {
-        int count = 0;
-        queue<int> elements;
-        for(int i = 0; i < vertices; i++) {
-            if(!indegree[i]) {
-                elements.push(i);
+    bool detectCycle(vector<vector<int>>& graph, vector<int>& indegree) {
+        int courses = graph.size();
+        queue<int> q;
+        vector<int> topoSort;
+        
+        for(int i = 0; i < courses; i++) {
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        while(!q.empty()) {
+            int parent = q.front();
+            q.pop();
+            topoSort.push_back(parent);
+            for(int child : graph[parent]) {
+                indegree[child]--;
+                if(indegree[child] == 0)
+                    q.push(child);
             }
         }
-        while(!elements.empty()) {
-            int top = elements.front();
-            elements.pop();
-            count++;
-            for(int connection : graph[top]) {
-                indegree[connection]--;
-                if(!indegree[connection])
-                    elements.push(connection);
-            }
-        }
-        return count == vertices;
+        return topoSort.size() == indegree.size();        
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses);
-        vector<bool> visited(numCourses);
         vector<vector<int>> graph(numCourses);
+        vector<int> inDegree(numCourses);
         
         for(auto& prerequisite : prerequisites) {
-            graph[prerequisite[1]].push_back(prerequisite[0]);
-            indegree[prerequisite[0]]++;
+            int parent = prerequisite[0];
+            int child = prerequisite[1];
+            
+            graph[parent].push_back(child);
+            inDegree[child]++;
         }
         
-        return topologicalSort(graph, numCourses, indegree);
+        return detectCycle(graph, inDegree);
+        
     }
 };
