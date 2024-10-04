b20b023d310e78feef3d7c60f26d1a1fbe1f14cd
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-10-04 10:20:49 +0530

    Time: 83 ms (5.40%), Space: 59.2 MB (7.40%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    #define all(skill) skill.begin(), skill.end(), 0
+public:
+    long long dividePlayers(vector<int>& skill) {
+        unordered_map<int, int> skillMap;
+        long long teamChemistry = 0;
+        int n = skill.size();
+        int totalSkill = accumulate(all(skill));
+        
+        if(totalSkill % (n / 2) != 0)
+            return -1;
+        
+        int targetSkill = totalSkill / (n / 2);
+        
+        for(int& playerSkill : skill) {
+            int currentTarget = targetSkill - playerSkill;
+            
+            if(skillMap.find(currentTarget) != skillMap.end()) {
+                skillMap[currentTarget]--;
+                if(skillMap[currentTarget] == 0)
+                    skillMap.erase(currentTarget);
+                teamChemistry += ((long long)currentTarget * playerSkill);
+            } else {
+                skillMap[playerSkill]++;
+            }
+        }
+        if(!skillMap.empty())
+            return -1;
+        return teamChemistry;
+    }
+};
