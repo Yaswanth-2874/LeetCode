63932ba8fc86e7e69f0698dcc56287438b4b8125
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-10-09 20:20:09 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,15 +1,20 @@
 class Solution {
+    // OPTIMISATION IN O(1) SPACE
 public:
     int minAddToMakeValid(string s) {
-        stack<char> brackets;
+        int openingBrackets = 0;
+        int requiredAdditions = 0;
         for(auto& bracket : s) {
-            if(bracket == ')')
-                if(!brackets.empty() && brackets.top() == '(') {
-                    brackets.pop();
-                    continue;
-                }
-            brackets.push(bracket);
+            if(bracket == '(')
+                openingBrackets++;
+            else {
+                if(openingBrackets)
+                    openingBrackets--;
+                else
+                    requiredAdditions++;
+            }
         }
-        return brackets.size();
+        // FOR ALL OPENING BRACKETS ADD ONE CLOSING BRACKET
+        return requiredAdditions + openingBrackets;
     }
 };
