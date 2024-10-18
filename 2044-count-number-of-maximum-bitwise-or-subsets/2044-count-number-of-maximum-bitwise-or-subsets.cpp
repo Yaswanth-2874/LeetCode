d917f6c9831126a82422875d1ea2693ccb9638e2
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-10-18 20:51:47 +0530

    Time: 119 ms (12.36%), Space: 309.2 MB (7.27%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,14 +1,20 @@
 class Solution {
     // MAX BITWISE OR IS DEFINITELY RESULT OF BITWISE OR ON EVERY ELEMENT
     // SINCE CONSTRAINTS ARE QUITE SMALL DO PICK NOT PICK (no need of dp also)
+    // NOW ILL USE MEMO
+    
+    vector<vector<int>> memo;
+    
     int findNumberOfWays(vector<int>& nums, int targetOR, int index, int currentOR) {
         if(index == nums.size())
             return currentOR == targetOR;
+        if(memo[index][currentOR] != -1)
+            return memo[index][currentOR];
                 
         int pick = findNumberOfWays(nums, targetOR, index+1, currentOR | nums[index]);
         int dontPick = findNumberOfWays(nums, targetOR, index+1, currentOR);
         
-        return pick + dontPick;
+        return memo[index][currentOR] = pick + dontPick;
     }
 public:
     int countMaxOrSubsets(vector<int>& nums) {
@@ -16,6 +22,8 @@ public:
         for(int& num : nums) {
             maxORValue |= num;
         }
+        
+        memo = vector<vector<int>> (nums.size(), vector<int> (maxORValue + 1, -1));
         return findNumberOfWays(nums, maxORValue, 0, 0);
     }
 };
