d3d3a86cdb2ddf6321abe4d896ee0e520af717e6
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-10-18 23:23:45 +0530

    Time: 30 ms (49.73%), Space: 18.7 MB (37.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,73 @@
 class Solution {
-    int m, n;
+    class DisjointSet {
+        vector<int> parent, size;
+        int components;
+    public:
+        DisjointSet(int n) {
+            size.resize(n, 1);
+            for(int i = 0; i < n; i++)
+                parent.push_back(i);
+            components = 0;
+        }
+        int findParent(int node) {
+            if(node == parent[node])
+                return node;
+            return parent[node] = findParent(parent[node]);
+        }
+        bool unionMerge(int nodeA, int nodeB) {
+            int parentA = findParent(nodeA);
+            int parentB = findParent(nodeB);
+            
+            if(parentA == parentB)
+                return false;
+            
+            components--;            
+            if(size[parentA] >= size[parentB]) {
+                parent[parentB] = parentA;
+                size[parentA] += size[parentB];
+            } else {
+                parent[parentA] = parentB;
+                size[parentB] += size[parentA];
+            }
+            return true;
+            
+        }
+    };
+    int n, m;
     vector<pair<int, int>> directions;
     
-    void visitIsland(vector<vector<char>>& grid, int i, int j) {
-        if(i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == '0')
-            return;
-        
-        // cout<<grid[i][j]<<" : ";
-        // cout<<"Visited : "<<i<<", "<<j<<endl;
-        grid[i][j] = '0';
-        
-        for(auto& [x, y] : directions) {
-            visitIsland(grid, i+x, j+y);
-        }        
+    bool mergeNeighbours(int x, int y, vector<vector<char>>& grid, DisjointSet &ds) {
+        if(x >= n || x < 0 || y >= m || y < 0 || grid[x][y] == '0' || grid[x][y] == 'V')
+            return false;
+        int gridAddress = x * m + y;
+        grid[x][y] = 'V';
+        for(auto& [xInc, yInc] : directions) {
+            if(mergeNeighbours(x + xInc, y + yInc, grid, ds)) {
+                int newGridAddress = (x + xInc) * m + (y + yInc);
+                ds.unionMerge(gridAddress, newGridAddress);
+            }
+        }
+        return true;
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        n = grid.size();
+        m = grid[0].size();
+        directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        DisjointSet ds(n * m);
         
-        directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mergeNeighbours(i, j, grid, ds);
+            }
+        }
         int islands = 0;
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1') {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int address = i * m + j;
+                if(grid[i][j] != '0' && ds.findParent(address) == address)
                     islands++;
-                    visitIsland(grid, i , j);
-                }
             }
         }
         return islands;
