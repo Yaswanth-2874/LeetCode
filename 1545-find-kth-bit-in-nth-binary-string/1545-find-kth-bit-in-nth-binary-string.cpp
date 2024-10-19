93a7e271220e0dff34275e77dc9f7d091c3d6d54
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-10-19 15:19:20 +0530

    Time: 78 ms (18.43%), Space: 44.3 MB (50.34%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,24 +1,19 @@
 class Solution {
-    // CONSTRAINTS ARE LESS, EVEN BRUTE FORCE SIMULATION WILL GET ACCEPTED
-    // WILL OPTIMISE AFTER BRUTE FORCING
-    string reverseInvert(string s) {
-        reverse(s.begin(), s.end());
-        string& reversed = s;
-        for(auto& ch : reversed) {
-            ch = ch == '1' ? '0' : '1';
+    string generateString(int n) {
+        if(n == 1)
+            return "0";
+        string si = generateString(n-1);
+        si.push_back('1');
+        for(int i = si.size() - 2; i >= 0; i--) {
+            char inverted = si[i] == '0' ? '1' : '0';
+            si.push_back(inverted);
         }
-        return reversed;
+        return si;
     }
 public:
     char findKthBit(int n, int k) {
-        string si = "0";
-        for(int i = 1; i < n; i++) {
-            string newString = si;
-            newString.push_back('1');
-            newString += reverseInvert(si);
-            si = newString;
-        }
-        // cout<<si<<endl;
-        return si[k-1];
+        string nthString = generateString(n);
+        // cout<<nthString<<endl;
+        return nthString[k-1];
     }
 };
