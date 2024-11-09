d52385688ac127b6ebc16033f58ee4edd10afd5e
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-11-09 10:21:36 +0530

    Time: 17 ms (88.08%), Space: 16.5 MB (72.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,18 @@
 class Solution {
-    vector<int> memo;
-    
-    int findMinCoins(int amount, vector<int>& coins) {
-        if(amount == 0)
-            return 0;
-        if(amount < 0)
-            return -1;
-        if(memo[amount] != -2)
-            return memo[amount];
-        
-        int minCoins = INT_MAX;
-        for(int i = coins.size() - 1; i >= 0; i--) {
-            int requiredCoins = findMinCoins(amount - coins[i], coins);
-            if(requiredCoins == -1)
-                continue;
-            minCoins = min(minCoins, 1 + requiredCoins);
-        }
-        return memo[amount] = minCoins == INT_MAX ? -1 : minCoins;        
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        memo = vector<int> (amount + 1, -2);
-        if(amount == 0)
-            return 0;
-        return findMinCoins(amount, coins);
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+        
+        for(int currentAmount = 1; currentAmount <= amount; currentAmount++) {
+            for(int& coin : coins) {
+                int balance = currentAmount - coin;
+                if(balance < 0 || dp[balance] == INT_MAX)
+                    continue;
+                dp[currentAmount] = min(1+dp[balance], dp[currentAmount]);
+            }
+        }
+        
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
