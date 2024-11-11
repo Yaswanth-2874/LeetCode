945a6ddab9ef6865714599519843584e61baf8e2
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-11-11 23:59:21 +0530

    Time: 40 ms (18.95%), Space: 69.1 MB (78.62%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,35 +1,51 @@
 class Solution {
-    vector<pair<int, int>> directions;
-    int rows, columns;
-    vector<vector<int>> memo;
+    // Bottom up successful, next target is to space optimise this
     
-    int findMaxPath(int x, int y, int prevVal, vector<vector<int>>& grid) {
-        if(x >= rows || y >= columns || x < 0 || y < 0 || grid[x][y] <= prevVal)
-            return 0;
-        if(memo[x][y] != -1)
-            return memo[x][y];
-        
-        int maxDistance = 0;
-        
-        for(auto& [xInc, yInc] : directions) {
-            maxDistance = max(maxDistance, findMaxPath(x + xInc, y + yInc, grid[x][y], grid));
-        }
-        
-        return memo[x][y] = 1 + maxDistance;
-    }
-    
+    // This step seems easy as i can see that i am only using dp of nextCol, i can simply convert it into a array with only 2 cols
 public:
     int maxMoves(vector<vector<int>>& grid) {
-        directions = {{-1, 1}, {0, 1}, {1, 1}};
-        rows = grid.size();
-        columns = grid[0].size();
-        int maxMoves = 0;
-        memo = vector<vector<int>> (rows, vector<int> (columns, -1));
+        int rows = grid.size();
+        int columns = grid[0].size();
         
-        for(int i = 0; i < rows; i++) {
-            int moves = findMaxPath(i, 0, -1, grid);
-            maxMoves = max(moves-1, maxMoves);
+        vector<vector<int>> dp(rows, vector<int> (2, 0));
+        
+        for(int col = columns - 2; col >= 0; col--) {
+            int nextCol = col + 1;
+            
+            for(int row = 0; row < rows; row++) {
+                int topRight = INT_MIN;
+                int middleRight = grid[row][nextCol];
+                int bottomRight = INT_MIN;
+                
+                if(row - 1 >= 0) {
+                    topRight = grid[row - 1][nextCol];
+                } if(row + 1 < rows) {
+                    bottomRight = grid[row + 1][nextCol];
+                }
+                
+                int currentValue = grid[row][col];
+                int maxDistance = 0;
+                
+                if(topRight > currentValue) 
+                    maxDistance = max(maxDistance, 1+dp[row-1][1]);
+                if(bottomRight > currentValue) 
+                    maxDistance = max(maxDistance, 1+dp[row+1][1]);
+                if(middleRight > currentValue)
+                    maxDistance = max(maxDistance, 1+dp[row][1]);
+                
+                dp[row][0] = maxDistance;
+            }
+            
+            for(int row = 0; row < rows; row++) {
+                dp[row][1] = dp[row][0];
+            }
         }
-        return maxMoves;
+        
+        int maxDistance = 0;
+        for(int i = 0; i < rows; i++) {
+            maxDistance = max(maxDistance, dp[i][0]);
+        }
+        
+        return maxDistance;        
     }
 };
