74315c57af22233ca51bd454caf821d19bf092fa
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-11-12 22:23:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (62.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    int findNumberOfPaths(vector<vector<int>>& memo, int m, int n, int i = 0, int j = 0) {
-        if(i == m - 1 && j == n - 1) 
-            return memo[i][j] = 1;
-        
-        if(i >= m || j >= n)
-            return 0;
-        
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        
-        int left = findNumberOfPaths(memo, m, n, i, j+1);
-        int down = findNumberOfPaths(memo, m, n, i+1, j);
-        
-        return memo[i][j] = left+down;
-    }
+    // Came for bottom up
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int> (n, -1));
-        return findNumberOfPaths(memo, m, n);
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+        // my destination is [m-1][n-1];
+        dp[m-1][n-1] = 1;
+        
+        for(int row = m-1; row >= 0; row--) {
+            for(int col = n-1; col >= 0; col--) {
+                dp[row][col] = max(dp[row][col], dp[row + 1][col] + dp[row][col + 1]);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
