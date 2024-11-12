64cf0b707189bdedd33e74100d049e2690ba6e0e
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-11-12 22:39:32 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (74.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,21 @@
 class Solution {
-    // Came for bottom up
+    // Space Optimised
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
-        // my destination is [m-1][n-1];
-        dp[m-1][n-1] = 1;
+        vector<int> lastRow(n+1, 0);
+        vector<int> currentRow(n+1, 0);
+        lastRow[n-1] = 1;
         
         for(int row = m-1; row >= 0; row--) {
             for(int col = n-1; col >= 0; col--) {
-                dp[row][col] = max(dp[row][col], dp[row + 1][col] + dp[row][col + 1]);
+                currentRow[col] = max(currentRow[col], lastRow[col]+currentRow[col+1]);
+            }
+            for(int i = 0; i < lastRow.size(); i++) {
+                lastRow[i] = currentRow[i];
+                currentRow[i] = 0;
             }
         }
         
-        return dp[0][0];
+        return lastRow[0];
     }
 };
