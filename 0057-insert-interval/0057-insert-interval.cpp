51827d1caa59a161ad7b00a26bb930073a6b364b
Bora Yaswanth Reddy <139699536+Yaswanth-2874@users.noreply.github.com>
2024-11-29 20:19:15 +0530

    Time: 0 ms (100.00%), Space: 21.9 MB (6.64%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    // On my way to optimise this shitty code
+    // As i promised, here's the better version of my previous approach
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         if(!intervals.size())
@@ -7,30 +7,34 @@ public:
         
         int newIntervalStart = newInterval[0];
         int newIntervalEnd = newInterval[1];
-        
-        intervals.push_back({newIntervalStart, newIntervalEnd});
-        sort(intervals.begin(), intervals.end());
-        
         vector<vector<int>> newIntervals;
-        
-        int start = intervals[0][0];
-        int end = intervals[0][1];
         int n = intervals.size();
+        bool inserted = false;
         
         for(int i = 0; i < n; i++) {
-            int commonStart = max(start, intervals[i][0]);
-            int commonEnd = min(end, intervals[i][1]);
+            int start = intervals[i][0];
+            int end = intervals[i][1];
             
-            if(commonStart > commonEnd) {
+            if(newIntervalStart > end || start > newIntervalEnd) {
+                if(!inserted && start > newIntervalEnd) {
+                    inserted = true;
+                    newIntervals.push_back({newIntervalStart, newIntervalEnd});
+                }
                 newIntervals.push_back({start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
-            } else {
-                end = max(end, intervals[i][1]);
+                continue;
+            }
+            
+            newIntervalStart = min(start, newIntervalStart);
+            newIntervalEnd = max(end, newIntervalEnd);
+            
+            if(i + 1 >= n || newIntervalEnd < intervals[i+1][0]) {
+                inserted = true;
+                newIntervals.push_back({newIntervalStart, newIntervalEnd});
             }
         }
+        if(!inserted)
+            newIntervals.push_back({newIntervalStart, newIntervalEnd});
         
-        newIntervals.push_back({start, end});
         return newIntervals;
     }
 };
